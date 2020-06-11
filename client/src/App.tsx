829d3d8207bf541407e2299a35ba084e5ac30b5c
Ankit <ankuradhey@gmail.com>
2020-06-11 13:45:32 +0530

    Routing correction

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ToastProvider } from "react-toast-notifications";
 import { Home, Post, Create } from "./pages";
 import { BaseLayout } from "./components/templates/BaseLayout";
 
 function App() {
     return (
         <Router>
-            <BaseLayout>
-                <main>
-                    <Switch>
-                        <Route exact path="/" component={Home} />
-                        <Route exact path="/:slug" component={Post} />
-                        <Route exact path="/create" component={Create} />
-                    </Switch>
-                </main>
-            </BaseLayout>
+            <ToastProvider>
+                <BaseLayout>
+                    <main>
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route exact path="/create" component={Create} />
+                            <Route exact path="/:slug" component={Post} />
+                        </Switch>
+                    </main>
+                </BaseLayout>
+            </ToastProvider>
         </Router>
     );
 }
